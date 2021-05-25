9d3127af824c39b77ccdf96bee8be8eef049c5ad
minasmeker <84793073+minasmeker@users.noreply.github.com>
2021-05-25 17:30:38 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    basic.showString("ZDRAVO")
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
